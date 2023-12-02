06ba82196557f5a18470fe2ea27747d8f7a183e8
WhiteThunder <whetethunger@gmail.com>
2023-12-01 17:10:54 -0800

    Fix fluid and industrial combiners being unintentionally affected

--- BetterRootCombiners.cs
+++ BetterRootCombiners.cs
@@ -1,10 +1,12 @@
 ﻿namespace Oxide.Plugins
 {
-    [Info("Better Root Combiners", "WhiteThunder", "1.0.0")]
+    [Info("Better Root Combiners", "WhiteThunder", "1.0.1")]
     [Description("Allows root combiners to accept input from any electrical source.")]
     internal class BetterRootCombiners : CovalencePlugin
     {
         private const string PermissionUse = "betterrootcombiners.use";
+        private const string RootCombinerPrefab = "assets/prefabs/deployable/playerioents/gates/combiner/electrical.combiner.deployed.prefab";
+        private uint RootCombinerPrefabId;
 
         private void Init()
         {
@@ -14,11 +16,22 @@
 
         private void OnServerInitialized()
         {
+            RootCombinerPrefabId = GameManager.server.FindPrefab(RootCombinerPrefab)
+                ?.GetComponent<ElectricalCombiner>()?.prefabID ?? 0;
+
+            if (RootCombinerPrefabId == 0)
+            {
+                LogError($"Unable to determine prefabID of {RootCombinerPrefab}");
+                return;
+            }
+
             foreach (var entity in BaseNetworkable.serverEntities)
             {
-                var combiner = entity as ElectricalCombiner;
-                if (combiner != null)
-                    OnEntitySpawned(combiner);
+                var rootCombiner = entity as ElectricalCombiner;
+                if (rootCombiner == null)
+                    continue;
+
+                OnEntitySpawned(rootCombiner);
             }
 
             Subscribe(nameof(OnEntitySpawned));
@@ -26,11 +39,16 @@
 
         private void OnEntitySpawned(ElectricalCombiner combiner)
         {
+            if (combiner.prefabID != RootCombinerPrefabId)
+                return;
+
             var rootConnectionsOnly = combiner.OwnerID == 0
                 || !permission.UserHasPermission(combiner.OwnerID.ToString(), PermissionUse);
 
             foreach (var input in combiner.inputs)
+            {
                 input.rootConnectionsOnly = rootConnectionsOnly;
+            }
         }
     }
 }
